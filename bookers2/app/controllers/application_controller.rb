393b480a116a0258a27a58e7e5d707a95a57f329
Nebel-hb <nebel.hb@gmail.com>
2021-05-03 15:31:05 +0000

    [finish]

--- bookers2/app/controllers/application_controller.rb
+++ bookers2/app/controllers/application_controller.rb
@@ -16,6 +16,18 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
   end
 
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [
+    :email,
+    :name,
+    :postcode,
+    :prefecture_name,
+    :address_city,
+    :address_street,
+    :address_building
+  ])
+  end
+
 
 end
 
