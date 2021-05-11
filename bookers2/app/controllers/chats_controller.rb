f5ccb9e24120771a1da1a89a1552bd4aba9bf9df
Nebel-hb <nebel.hb@gmail.com>
2021-05-11 05:36:44 +0000

    5/11

--- bookers2/app/controllers/chats_controller.rb
+++ bookers2/app/controllers/chats_controller.rb
@@ -2,6 +2,8 @@ class ChatsController < ApplicationController
    def show
     @user = User.find(params[:id])
     rooms = current_user.user_rooms.pluck(:room_id)
+    puts "==============================="
+    p rooms
     user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
 
     if user_rooms.nil?
